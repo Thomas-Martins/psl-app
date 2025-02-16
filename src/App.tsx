3ad0df9a7243158ff28fa5ac65f2205048fee514
Thomas Martins <thomas.martins1710@gmail.com>
2025-02-16 21:50:25 +0100

    Initial Commit 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css'
+import { Button } from "@heroui/react"
 
 function App() {
 
   return (
-    <div>App</div>
+    <div>
+      <h1 className="text-3xl text-red-500">App</h1>
+      <Button color="primary">Click me</Button>
+    </div>
   )
 }
 
