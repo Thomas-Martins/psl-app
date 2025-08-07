1138b880045083d95e4965b0fbf11ab0eb19d45a
Thomas Martins <thomas.martins1710@gmail.com>
2025-08-07 22:36:56 +0200

    feat: wrap CartLayout in ProductViewProvider for context support

--- src/layouts/ShopLayout/CartLayout.tsx
+++ src/layouts/ShopLayout/CartLayout.tsx
@@ -1,13 +1,16 @@
+import { ProductViewProvider } from "@/contexts/Products/ProductViewContext";
 import Navbar from "@layouts/ShopLayout/Navbar.tsx";
 import { Outlet } from "react-router";
 
 export default function CartLayout() {
     return (
-        <div className="relative z-10 px-3 md:px-6 lg:px-8 min-h-screen">
-            <Navbar />
-            <div className="mb-5 max-w-screen-xl mx-auto mt-5 ">
-                <Outlet />
+        <ProductViewProvider>
+            <div className="relative z-10 px-3 md:px-6 lg:px-8 min-h-screen">
+                <Navbar />
+                <div className="mb-5 max-w-screen-xl mx-auto mt-5 ">
+                    <Outlet />
+                </div>
             </div>
-        </div>
+        </ProductViewProvider>
     );
 }
